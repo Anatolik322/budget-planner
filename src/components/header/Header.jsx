48229f08caa452af77579fa4629de32296a259dc
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-08-05 15:12:05 +0300

    style: styled header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './header.css'
+import coins from "../../img/pngwing.com.png"
 function Header() {
   return (
     <header className='header'>
-        <div className="logo"></div>
-        <div className="link"></div>
+        <div className="logo"> <img src={coins} alt="coins" /> <span className="header_text">Budget planner</span></div>
+        <div className="link"> <span className='header_link'>Stats</span></div>
     </header>
   )
 }
