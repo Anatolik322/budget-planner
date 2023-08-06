a50d4c3ee7e6e0d3b96138afbf5194a38dca1395
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-08-06 15:14:18 +0300

    feat: created table component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './index.css'
 import Header from "./components/header/Header";
+import Calendar from "./components/calendar/Calendar";
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Calendar></Calendar>
     </div>
   );
 }
--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import "./calendar.css"
 
 function Calendar() {
+  let d = new Date()
+  Date.prototype.daysInMonth = function() {
+		let days = 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
+    let daysArr = []
+    for(let i = 1; i <= days; i++){
+      daysArr.push(i)
+    }
+    return daysArr;
+	};
+  console.log(d.daysInMonth(), d.getDate())
+  const rows = [];
+  for (let i = 0; i < d.daysInMonth().length; i += 7) {
+    const rowCells = d.daysInMonth().slice(i, i + 7);
+    rows.push(
+      <tr key={i}>
+        {rowCells.map((cellData, index) => (
+          <td key={index}>{cellData}</td>
+        ))}
+      </tr>
+    );
+  }
   return (
     <div>
-      
+      <table class="table table-bordered text-center">
+        
+        <tbody>
+          <tr>
+            {
+              // d.daysInMonth().map((e) => {
+              //   if(e % 7 == 0){
+              //     return <tr>
+                       
+              //     </tr>
+                   
+              //   }
+              //   return <td>{e}</td>
+              // })
+              rows
+            }
+          </tr>
+        </tbody>
+        </table>
     </div>
   )
 }
