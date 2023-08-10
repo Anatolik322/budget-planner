acc1465eb86be583264fdc563988050194e7df33
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-08-10 15:10:22 +0300

    feat: added react-router-dom

--- src/index.js
+++ src/index.js
@@ -5,15 +5,29 @@ import App from './App';
 import  store, {persistor } from './service/store';
 import { Provider } from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App></App>,
+    },
+    {
+        path: "/budget-planner",
+        element: <App></App>,
+    },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+
  <Provider store={store}>
     <PersistGate loading = {null} persistor={persistor}>
-        <App />
+        <RouterProvider router={router} />
     </PersistGate>
-    
  </Provider>
    
 
