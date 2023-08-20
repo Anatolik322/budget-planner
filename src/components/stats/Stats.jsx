aad7eb5399c9c8711406fae9111649d9beaf4d4d
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-08-20 18:54:23 +0300

    style: styled stats component

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -3,23 +3,26 @@ import './stats.css'
 import { Chart } from "react-google-charts";
 import { useSelector } from 'react-redux';
 import Header from '../header/Header';
+import Footer from '../footer/Footer'
 function Stats() {
     
     const data = useSelector(state => state.categories)
     const options = {
-        title: "My Daily Activities",
+        title: "My expenses",
     };
 
     return (
     <div className='stats'>
         <Header></Header>
         <Chart
+        className='chart'
         chartType="PieChart"
         data={data}
         options={options}
-        width={"100%"}
+        width={"60%"}
         height={"400px"}
         />
+        <Footer></Footer>
     </div>
   )
 }
